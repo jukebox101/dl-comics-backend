608f7df1fe567f52d1d48682ddb74b187d5d1c60
jukebox101 <leonora.squires@gmail.com>
2020-07-25 11:25:10 -0400

    add serializer and seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_220446) do
+ActiveRecord::Schema.define(version: 2020_07_24_223441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2020_07_24_220446) do
   create_table "comics", force: :cascade do |t|
     t.string "title"
     t.integer "price"
-    t.string "category"
     t.string "genre"
     t.string "author"
     t.string "release_date"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Comic.destroy_all
+
+3.times do
+    User.create(
+        username: Faker::Twitter.screen_name,
+        password: 123
+    )
+
+    Comic.create(
+        title: Faker::DcComics.title,
+        price: 10,
+        genre: Faker::Book.genre,
+        author: Faker::Book.author,
+        release_date: "2019-09-01",
+        description: Faker::Lorem.paragraph,
+        publisher: Faker::Book.publisher
+    )
+end
