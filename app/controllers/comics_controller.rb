cfb14a139e983ab0a4f6313b2e663954a9acdd97
jukebox101 <leonora.squires@gmail.com>
2020-07-25 12:00:12 -0400

    added comic index and show routes/controllers

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,2 +1,12 @@
 class ComicsController < ApplicationController
+    def index
+        @comics = Comic.all
+        render json: @comics
+    end
+
+    def show
+        comic = Comic.find_by(id: params[:id])
+        render json: comic
+    end
+
 end
