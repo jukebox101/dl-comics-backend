1ef7baaaa705971626ac7d87e9eae0962a53921d
darren9897 <darrenkoomson17@gmail.com>
2020-07-29 11:40:18 -0400

    cleaned up user routes and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,32 @@
 class UsersController < ApplicationController
 
-    def index
-        users = User.all
-        render json: users
-    end
-
-    def show
-        user = User.find(params[:id])
-        render json: user
-    end
-
     def create
         user = User.create(new_user_params)
-        render json: user
-     end
 
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: { message: user.errors.full_messages }, status: :bad_request
+        end
+        
+    end
+
+    def login
+        user = User.find_by(username: params[:username])
+
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: { message: "Invalid username or password"}, status: :unauthorized
+        end
+    end
+
+    def logout
+        session.delete(:user_id)
+        render json: { message: "Logged Out" }
+    end
 
     private
 
